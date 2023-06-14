c3df4a38cdc9f45820beeccbdae05efc5375d121
nicolangelova <nicol.angelovaa@gmail.com>
2023-06-14 13:12:11 +0300

    Identity

--- Expense Tracker/Expense Tracker/Program.cs
+++ Expense Tracker/Expense Tracker/Program.cs
@@ -1,14 +1,21 @@
 using Expense_Tracker.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Expense_Tracker.Data;
+using Expense_Tracker.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 //DI
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<Expense_TrackerDbContext>();
 
 //Register Syncfusion license
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjM3NjUxOUAzMjMxMmUzMDJlMzBtMXE1YThVWDlsQ3lrNHU1V3JiQWJ1amhINzNVTnVDZkRqbnVyT2xyMFZ3PQ==;Mgo+DSMBaFt+QHJqVk1mQ1BDaV1CX2BZdllzRmlaeU4BCV5EYF5SRHBeQl1rTH1QcENhW3k=;Mgo+DSMBMAY9C3t2VFhiQlJPcEBDWnxLflF1VWtTel16d1NWESFaRnZdQV1lSH5TfkBgWnlYc3JT;Mgo+DSMBPh8sVXJ1S0R+X1pCaV5FQmFJfFBmTWlcfFRzckU3HVdTRHRcQltiSX5bc0ZgX3lbcXI=;MjM3NjUyM0AzMjMxMmUzMDJlMzBWbG53ZUdyMkZLVVlzTDN0NlZHb2VzT2gyY0pvUEdwMGFJbFpKV1FtYURVPQ==;NRAiBiAaIQQuGjN/V0d+Xk9HfVldXGRWfFN0RnNRdVtzflZAcC0sT3RfQF5jT39Tdk1mWnxYdnxdQA==;ORg4AjUWIQA/Gnt2VFhiQlJPcEBDWnxLflF1VWtTel16d1NWESFaRnZdQV1lSH5TfkBgWnlZcHdT;MjM3NjUyNkAzMjMxMmUzMDJlMzBkRmY2eTZpZG05NWNGZzZock1zY1NjKy91QU5KRE5WWWVqMWVvbXZsV0lNPQ==;MjM3NjUyN0AzMjMxMmUzMDJlMzBLUjVvdWoyS2s3NTdUQUxrcHVncWdPcENzaWxKbktaU3VMYzVUek9IQjk4PQ==;MjM3NjUyOEAzMjMxMmUzMDJlMzBLKzlYTFZXcGRQVU1YYmZDRG92T3BLTzRwQUp1M1hFSlFZcWRPb05WQmtRPQ==;MjM3NjUyOUAzMjMxMmUzMDJlMzBTNk9zSzU0Vk9peXIzbFhxN1B6L1gzK1FEM2tLbmV0RktmY1NsemE1UWU0PQ==;MjM3NjUzMEAzMjMxMmUzMDJlMzBtMXE1YThVWDlsQ3lrNHU1V3JiQWJ1amhINzNVTnVDZkRqbnVyT2xyMFZ3PQ==");
@@ -23,11 +30,14 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+app.MapRazorPages();
+
 
 app.Run();
